98d9a8e78157fd702d3386e1fa12bcc6dfb457db
mehulkaushal29 <143791062+mehulkaushal29@users.noreply.github.com>
2024-10-08 01:48:07 +1100

    Update PhoneList.js

--- frontend/src/components/PhoneList.js
+++ frontend/src/components/PhoneList.js
@@ -11,8 +11,8 @@ function PhoneList(props) {
             <table onClick={(e) => e.stopPropagation()}>
                 <thead>
                     <tr>
-                        <th>Phone Type</th>
-                        <th>Phone Number</th>
+                        <th>Name</th>
+                        <th>Number</th>
                         <th></th>
                     </tr>
                 </thead>
